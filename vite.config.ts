6c8f444b89f3e54783e688251e1bfe72ba212511
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2026-04-20 17:33:21 -0300

    fix: corrige lugar

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@shared': path.resolve(__dirname, '../shared/src'),
+      '@shared': path.resolve(__dirname, '../cht-shared/src'),
     },
   },
 })
